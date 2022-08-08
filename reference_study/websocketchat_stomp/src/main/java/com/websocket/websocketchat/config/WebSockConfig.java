ea0a9424e4b51490cc456afa7cba6d5a0bfb2c72
Kang-SeoHyun <kang061020@gmail.com>
2022-08-08 16:03:13 +0900

    part2

---
+++ reference_study/websocketchat_stomp/src/main/java/com/websocket/websocketchat/config/WebSockConfig.java
@@ -0,0 +1,25 @@
+package com.websocket.websocketchat.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.*;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocketMessageBroker //stomp 사용하기 위해
+public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/sub");
+        config.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
+                .withSockJS();
+    }
+}
